42b43c8c723bf7ed7583484b92d83d7fc77bcf25
Fariha Fathima <farihafathima55@gmail.com>
2023-06-17 07:29:07 +0530

    java

--- NewJavaFeatureProject/src/cgg/gitprac/GitPracc.java
+++ NewJavaFeatureProject/src/cgg/gitprac/GitPracc.java
@@ -3,13 +3,10 @@ package cgg.gitprac;
 public class GitPracc {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("happy world");
 		System.out.println("----------------------------------");
-=======
 		System.out.println("world");
-		
->>>>>>> origin/master
+	
 
 	}
 
